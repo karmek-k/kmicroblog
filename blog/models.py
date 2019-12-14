f623159b2347ffe146cc65b6882d8f06bef071f8
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2019-12-14 20:43:43 +0100

    Create the blog app and Post model

---
+++ blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    content = models.TextField()
+    votes = models.IntegerField(default=0)
+    op = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content[:20]
+
--- kmicroblog/settings.py
+++ kmicroblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-jc&im@c2yid^v274=k*1p6(zr7c#17eb1x)3q(06vam&blhi*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.43.76']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
