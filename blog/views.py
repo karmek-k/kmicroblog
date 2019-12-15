9d10b89726d96a03f706537353cfb182c9a60197
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2019-12-15 20:34:20 +0100

    Add add_urls view

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 from .models import Post
+from .forms import PostForm
 
 
 def index(request):
     posts = Post.objects.order_by('-id')
     
     return render(request, 'blog/index.html', {'posts': posts})
+
+
+@login_required
+def add_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+        return redirect('blog:index')
+    
+    return render(request, 'blog/add_post.html', {'form': PostForm})
