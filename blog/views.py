701e436299530ec804526456669b966435f086f0
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2019-12-15 15:36:55 +0100

    Order post by descending id

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,6 @@ from .models import Post
 
 
 def index(request):
-    posts = Post.objects.all()
+    posts = Post.objects.order_by('-id')
     
     return render(request, 'blog/index.html', {'posts': posts})
