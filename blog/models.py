22a993cf3b27d996e1fe0a351bc789925aeb8eeb
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2019-12-28 21:21:03 +0100

    Add Tag model

--- blog/models.py
+++ blog/models.py
@@ -2,12 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=20, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     content = models.TextField()
     votes = models.IntegerField(default=0)
     op = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='OP')
     created_at = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField(Tag, related_name='tags')
 
     def __str__(self):
         return str(self.content)[:50]
-
