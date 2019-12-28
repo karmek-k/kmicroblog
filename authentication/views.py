a74e10db926c9be890ae084408107b2f2c0c6fd5
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2019-12-28 13:53:26 +0100

    Create signup.html

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('blog:index')
+
+    return render(request, 'registration/signup.html', {'form': UserCreationForm()})
