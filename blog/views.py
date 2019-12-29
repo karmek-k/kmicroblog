e4e3cd63bd65e8b0f4ef98a145e20e2b61b2d43d
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2019-12-29 16:36:46 +0100

    Pass posts along with their tags :)

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
-from .models import Post
+from .models import Post, Tag
 from .forms import PostForm
 
 
 def index(request):
-    posts = Post.objects.order_by('-id')
-    
-    return render(request, 'blog/index.html', {'posts': posts})
+    posts_tags = {}
+    for post in Post.objects.order_by('-id'):
+        # posts_tags:s
+        # {post1: (tag1, tag2, ...), post2: (), ...}
+        posts_tags.setdefault(post, tuple(post.tags.all()))
+
+    return render(request, 'blog/index.html', {'posts_tags': posts_tags.items()})
 
 
 @login_required
