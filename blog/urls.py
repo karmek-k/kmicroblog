d487d25f73c3517103f04feadaf9d5f1bfd5ee09
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-01-04 20:14:12 +0100

    Replace function-based views to class-based

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexListView.as_view(), name='index'),
     path('add_post/', views.add_post, name='add_post'),
-    path('tag/<tag_name>', views.tag, name='tag'),
+    path('tag/<tag_name>/', views.TagListView.as_view(), name='tag'),
 ]
