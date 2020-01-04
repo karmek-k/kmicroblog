b1e8712c57047054a2111e28946a34b79481e752
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-01-04 16:16:10 +0100

    Add tag view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.views import generic
 
 from .models import Post, Tag
 from .forms import PostForm
@@ -29,4 +30,10 @@ def add_post(request):
 
 
 def tag(request, tag_name):
-    pass
+    posts_tags = {}
+    for post in Post.objects.order_by('-id').filter(tags__name__iexact=tag_name):
+        # posts_tags:s
+        # {post1: (tag1, tag2, ...), post2: (), ...}
+        posts_tags.setdefault(post, tuple(post.tags.all()))
+
+    return render(request, 'blog/index.html', {'posts_tags': posts_tags.items()})
