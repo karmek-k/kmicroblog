c790c0d801066b85b2237ae493b2e4aeddf35ec5
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-01-15 12:44:48 +0100

    Add captcha to settings and urls

--- blog/forms.py
+++ blog/forms.py
@@ -1,10 +1,12 @@
 from django import forms
+from captcha.fields import CaptchaField
 
 from .models import Post
 
 
 class PostForm(forms.ModelForm):
     tags = forms.CharField(max_length=100, required=False)
+    captcha = CaptchaField()
 
     class Meta:
         model = Post
--- kmicroblog/urls.py
+++ kmicroblog/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('captcha/', include('captcha.urls')),
     path('', include('blog.urls')),
     path('accounts/', include('authentication.urls')),
 ]
