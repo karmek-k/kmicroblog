e600e60400a31b71dade17615e89975fca47f170
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-01-17 20:38:13 +0100

    Move ALLOWED_HOSTS to .env

--- kmicroblog/settings.py
+++ kmicroblog/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'secret')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.getenv('DEBUG', 0))
 
-ALLOWED_HOSTS = ['192.168.43.76', 'localhost']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split()
 
 
 # Application definition
