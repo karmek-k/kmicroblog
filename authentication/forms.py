4d5c330bdcaae308f3f8a775d8cfe7353e326665
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-01-29 17:43:10 +0100

    Update CaptchaUser CreationForm

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,7 +1,12 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
 
 from captcha.fields import CaptchaField
 
 
 class CaptchaUserCreationForm(UserCreationForm):
     captcha = CaptchaField()
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username']
